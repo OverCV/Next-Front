58ce8847f3e01624ad25cbdd976fa76a27a3f689
Over.HCV <over.castrillon22396@ucaldas.edu.co>
2025-03-31 15:51:13 -0500

    feat: Commenting errors

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,45 +1,45 @@
-// app\patients\[userId]\new-appointment\page.tsx
+// // app\patients\[userId]\new-appointment\page.tsx
 
-import Image from "next/image";
+// import Image from "next/image";
 
-import { AppointmentForm } from "@/src/components/forms/AppointmentForm";
+// import { AppointmentForm } from "@/src/components/forms/AppointmentForm";
 
-import { getPatient } from "@/src/lib/actions/patient.actionsions";
+// import { getPatient } from "@/src/lib/actions/patient.actionsions";
 
-const Appointment = async ({ params: { userId } }: SearchParamProps) => {
-  const patient = await getPatient(userId);
+// const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+//   const patient = await getPatient(userId);
 
-  return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image
-            src="/assets/brand/logo-black.ico"
-            height={1000}
-            width={1000}
-            alt="logo"
-            className="mb-12 h-10 w-fit"
-          />
+//   return (
+//     <div className="flex h-screen max-h-screen">
+//       <section className="remove-scrollbar container my-auto">
+//         <div className="sub-container max-w-[860px] flex-1 justify-between">
+//           <Image
+//             src="/assets/brand/logo-black.ico"
+//             height={1000}
+//             width={1000}
+//             alt="logo"
+//             className="mb-12 h-10 w-fit"
+//           />
 
-          <AppointmentForm
-            patientId={patient?.$id}
-            userId={userId}
-            type="create"
-          />
+//           <AppointmentForm
+//             patientId={patient?.$id}
+//             userId={userId}
+//             type="create"
+//           />
 
-          <p className="copyright mt-10 py-12">© 2025 CarePluse</p>
-        </div>
-      </section>
+//           <p className="copyright mt-10 py-12">© 2025 CarePluse</p>
+//         </div>
+//       </section>
 
-      <Image
-        src="/assets/images/appointment-img.png"
-        height={1500}
-        width={1500}
-        alt="appointment"
-        className="side-img max-w-[390px] bg-bottom"
-      />
-    </div>
-  );
-};
+//       <Image
+//         src="/assets/images/appointment-img.png"
+//         height={1500}
+//         width={1500}
+//         alt="appointment"
+//         className="side-img max-w-[390px] bg-bottom"
+//       />
+//     </div>
+//   );
+// };
 
-export default Appointment;
+// export default Appointment;
--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,44 +1,44 @@
-// app\patients\[userId]\register\page.tsx
-import Image from "next/image";
-import { redirect } from "next/navigation";
+// // app\patients\[userId]\register\page.tsx
+// import Image from "next/image";
+// import { redirect } from "next/navigation";
 
-import RegistroForm from "@/src/components/forms/RegistroForm";
+// import RegistroForm from "@/src/components/forms/RegistroForm";
 
-import { getPatient, getUser } from "@/src/lib/actions/patient.actionsions";
+// import { getPatient, getUser } from "@/src/lib/actions/patient.actionsions";
 
-const Register = async ({ params: { userId } }: SearchParamProps) => {
-  const user = await getUser(userId);
-  const patient = await getPatient(userId);
+// const Register = async ({ params: { userId } }: SearchParamProps) => {
+//   const user = await getUser(userId);
+//   const patient = await getPatient(userId);
 
-  if (patient) redirect(`/patients/${userId}/new-appointment`);
+//   if (patient) redirect(`/patients/${userId}/new-appointment`);
 
-  return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-          <Image
-            src="/assets/brand/logo-white.ico"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+//   return (
+//     <div className="flex h-screen max-h-screen">
+//       <section className="remove-scrollbar container">
+//         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+//           <Image
+//             src="/assets/brand/logo-white.ico"
+//             height={1000}
+//             width={1000}
+//             alt="patient"
+//             className="mb-12 h-10 w-fit"
+//           />
 
-          <RegistroForm user={user} />
+//           <RegistroForm user={user} />
 
-          <p className="copyright py-12">© 2025 CarePluse</p>
-        </div>
-      </section>
+//           <p className="copyright py-12">© 2025 CarePluse</p>
+//         </div>
+//       </section>
 
-      <Image
-        src="/assets/images/register-img.png"
-        height={1000}
-        width={1000}
-        alt="patient"
-        className="side-img max-w-[390px]"
-      />
-    </div>
-  );
-};
+//       <Image
+//         src="/assets/images/register-img.png"
+//         height={1000}
+//         width={1000}
+//         alt="patient"
+//         className="side-img max-w-[390px]"
+//       />
+//     </div>
+//   );
+// };
 
-export default Register;
+// export default Register;
