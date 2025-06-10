046e7fdad698b4810759326e06da34ea506a99bf
camian <camian6@outlook.com>
2025-06-09 19:15:16 -0500

    feat:archivo para centralizar rutas api agregado

---
+++ src/services/RutasApi.ts
@@ -0,0 +1,8 @@
+// campampana
+export const CAMPANA_RUTA: string = "/campana/";
+
+// servicios medicos
+export const SERVICIO_MEDICO_RUTA: string = "/servicios-medicos/";
+
+// factor riesgo
+export const FACTOR_RIESGO_RUTA: string = "/factor-riesgo/";
