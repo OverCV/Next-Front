1474c6eaa93b49cb9240fe03ddc5851dba2fcc60
camian <camian6@outlook.com>
2025-06-10 17:41:00 -0500

    feat:rutas para servicios y factores campana added

--- src/services/RutasApi.ts
+++ src/services/RutasApi.ts
@@ -6,3 +6,9 @@ export const SERVICIO_MEDICO_RUTA: string = "/servicios-medicos";
 
 // factor riesgo
 export const FACTOR_RIESGO_RUTA: string = "/factor-riesgo";
+
+// asociar servicios medicos a una campaña
+export const CAMPANA_SERVICIOS_RUTA: string = "/servicios-campana";
+
+// asociar factores a una campaña
+export const CAMPANA_FACTORES_RUTA: string = "/campana-factores";
